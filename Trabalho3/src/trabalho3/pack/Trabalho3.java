bfebc29cd4e89cd274f89406fbcf999e9aa47995
Daniel F. Pettenon <danielpettenon@gmail.com>
2013-12-11 18:28:26 -0200

    Criação do pacote trabalho3.pack.

--- Trabalho3/src/trabalho3/pack/Trabalho3.java
+++ Trabalho3/src/trabalho3/pack/Trabalho3.java
@@ -2,17 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package trabalho3.pack;
 
 /**
  *
  * @author Daniel
  */
 public class Trabalho3 {
+    
+     public static void main(String[] args) {
+         
+     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
 }
