5a54f56e1176a22291ab1966944993406dbfe69d
Daniel F. Pettenon <danielpettenon@gmail.com>
2014-02-02 23:46:26 -0200

    Movimento finalizado, luz móvel adicionada.

--- Trabalho3/src/trabalho3/pack/Trabalho3.java
+++ Trabalho3/src/trabalho3/pack/Trabalho3.java
@@ -116,16 +116,17 @@ public class Trabalho3 extends JFrame
     //Carrega objetos.
     ObjectFile f = new ObjectFile(ObjectFile.RESIZE);
     Scene s = null;
-    ObjectFile f2 = new ObjectFile(ObjectFile.RESIZE);
     Scene s2 = null;
-    ObjectFile f3 = new ObjectFile(ObjectFile.RESIZE);
     Scene s3 = null;
+    Scene s4 = null; 
+    
     
     try
     {
       s = f.load("lp670.obj");
-      s2 = f2.load("gas.obj");
-      s3 = f3.load("Tree.obj");
+      s2 = f.load("gas.obj");
+      s3 = f.load("Tree.obj");
+      s4 = f.load("Tree.obj");
     }
     catch (Exception e)
     {
@@ -136,6 +137,18 @@ public class Trabalho3 extends JFrame
     tfVei.setTranslation(new Vector3f(0.0f,0f,0.0f));
     TransformGroup tgVei = new TransformGroup(tfVei);
     tgVei.addChild(s.getSceneGroup());  
+    
+    //Light no. 2: a point light.
+    Color3f lightColour2 = new Color3f(1.0f, 1.0f, 1.0f);
+    PointLight light2 = new PointLight(lightColour2,
+                                       new Point3f(0.0f,0.0f,0.0f),
+                                       new Point3f(0.2f,0.01f,0.01f));
+    light2.setInfluencingBounds(new BoundingSphere(new Point3d(0.0,0.0,0.0),Double.MAX_VALUE));
+    tgVei.addChild(light2);
+    
+    
+    
+   
     
     //Gas Station
     Transform3D tfGas = new Transform3D();
@@ -154,9 +167,16 @@ public class Trabalho3 extends JFrame
     tfTree.rotX(1.56);
     tfTree.setTranslation(new Vector3f(4.0f,-3.0f,0.7f));
     
+    Transform3D tfTree2 = new Transform3D();
+    tfTree2.rotX(1.56);
+    tfTree2.setTranslation(new Vector3f(-1.0f,-14.0f,0.7f));
+    
+    
     TransformGroup tgTree = new TransformGroup(tfTree);
+    TransformGroup tgTree2 = new TransformGroup(tfTree2);
     
     tgTree.addChild(s3.getSceneGroup()); 
+    tgTree2.addChild(s4.getSceneGroup()); 
     //In the following way, the names of the parts of the object can be
     //obtained. The names are printed.
     
@@ -178,13 +198,14 @@ public class Trabalho3 extends JFrame
       name2 = (String) enumer2.nextElement();
       //System.out.println("Name: "+name2);
     }  
+    //Tree
     Hashtable namedObjects3 = s3.getNamedObjects();
     Enumeration enumer3 = namedObjects3.keys();
     String name3;
     while (enumer3.hasMoreElements())
     {
       name3 = (String) enumer3.nextElement();
-      System.out.println("Name: "+name3);
+      //System.out.println("Name: "+name3);
     }  
     
     
@@ -345,7 +366,7 @@ public class Trabalho3 extends JFrame
     
     
    //Posicionamento do chão
-    Box groundGrass = new Box(100f,100f,1f,grassApp);
+    Box groundGrass = new Box(20f,20f,0.2f,grassApp);
     Box groundStone = new Box(3f,1f,0.001f,stoneApp);
 
     Transform3D groundGrassTrans = new Transform3D();
@@ -359,20 +380,50 @@ public class Trabalho3 extends JFrame
     tgGrass.addChild(groundGrass);
     tgStone.addChild(groundStone);
     
+    // Rampa e obstáculo
+    //Generate an Appearance for the cube.
+    Color3f ambientColourRamp = new Color3f(0.8f,0.0f,0.0f);
+    Color3f emissiveColourRamp = new Color3f(0.0f,0.0f,0.0f);
+    Color3f diffuseColourRamp = new Color3f(0.8f,0.0f,0.0f);
+    Color3f specularColourRamp = new Color3f(1.0f,0.0f,0.0f);
+    float shininessRamp = 10.0f;
     
- 
-
-//***Generate a sphere ***
+    Appearance rampApp = new Appearance();
+    rampApp.setMaterial(new Material(ambientColourRamp,emissiveColourRamp,diffuseColourRamp,specularColourRamp,shininessRamp));  
+     
+    Box myRamp = new Box(1.0f,1f,0.2f,rampApp);
+    Box myObs = new Box(2.0f,1f,0.2f,rampApp);
+     //Rotate and shift the cube slightly.
+    Transform3D tfRamp = new Transform3D();
+    Transform3D tfObs = new Transform3D();
+   
+    tfRamp.rotX(-0.4);
+    tfRamp.setTranslation(new Vector3f(0f,-7.2f,-0.1f));
+    
+    //tfObs.rotX(-0.4);
+    tfObs.setTranslation(new Vector3f(0f,-9.2f,-0.1f));
+    
+    TransformGroup tgRamp = new TransformGroup(tfRamp);
+    TransformGroup tgObs = new TransformGroup(tfObs);
+    
+    tgRamp.addChild(myRamp);
+    tgObs.addChild(myObs);
+    
+    //Generate a sphere ***
 
     //Generate an Appearance for the sphere.
     Color3f ambientColourSphere = new Color3f(0.2f,0.2f,0.0f);
     Color3f emissiveColourSphere = new Color3f(0.0f,0.0f,0.0f);
     Color3f diffuseColourSphere = new Color3f(0.4f,0.4f,0.0f);
     Color3f specularColourSphere = new Color3f(0.8f,0.8f,0.0f);
-    float shininessSphere = 200.0f;
+    float shininessSphere = 500.0f;
 
     Appearance sphereApp = new Appearance();
-
+    TransparencyAttributes ta1 = new TransparencyAttributes();
+    ta1.setTransparencyMode(TransparencyAttributes.BLENDED);
+    ta1.setTransparency(0.8f);
+    sphereApp.setTransparencyAttributes(ta1);
+    
     sphereApp.setMaterial(new Material(ambientColourSphere,emissiveColourSphere,
                            diffuseColourSphere,specularColourSphere,shininessSphere));
 
@@ -380,7 +431,7 @@ public class Trabalho3 extends JFrame
 
     Sphere mySphere = new Sphere(1.0f,Sphere.GENERATE_NORMALS,100,sphereApp);
     Transform3D tfSphere = new Transform3D();
-    tfSphere.setTranslation(new Vector3f(0.0f,0.0f,7.0f));
+    tfSphere.setTranslation(new Vector3f(0.0f,0.0f,5.0f));
 
     TransformGroup tgSphere = new TransformGroup(tfSphere);
     tgSphere.addChild(mySphere);
@@ -389,27 +440,76 @@ public class Trabalho3 extends JFrame
     
     
     //The Alpha for the car movment.
-    int timeMoveStart = 6000; //começa nos 6 segs
+    int timeMoveStart =4000; //começa nos 6 segs
     
     
-    Alpha carAlpha = new Alpha(1,Alpha.INCREASING_ENABLE+Alpha.DECREASING_ENABLE,
-                                      timeMoveStart,0,5000,5000,
-                                      5000,5000,5000,0);
-
+  // Alpha carAlpha = new Alpha(1,Alpha.INCREASING_ENABLE+Alpha.DECREASING_ENABLE,timeMoveStart,0,5000,5000,5000,5000,5000,0);
+    Alpha carAlpha = new Alpha(5,timeMoveStart,0,6000,2000,2000);
     //The car movement.
     
     Transform3D axisVei = new Transform3D();
-    //axisVei.rotX(-1.56);
     axisVei.rotZ(-1.56);
-    //axisVei.rotY(-1.56);
-    PositionInterpolator posIPCar = new PositionInterpolator(carAlpha,tgVei,axisVei,0.0f,20.0f);
+    
+    float[] knots = {0.0f, 0.05f, 0.1f,0.15f ,0.2f,0.25f,0.3f,0.35f,0.4f,0.45f,0.5f,0.55f,0.6f,0.65f,0.7f,0.75f,0.8f,0.85f,0.9f,0.95f,1f};
+    
+    Quat4f[] quats = new Quat4f[21];
+    quats[0] = new Quat4f( 0f, 0f, 0f, 1f);
+    quats[1] = new Quat4f( 0f, 0f, 0f, 1f);
+    quats[2] = new Quat4f( 0f, 0f, 0f, 1f);
+    quats[3] = new Quat4f( 0f, 0f, 0.0f, 1f);
+    quats[4] = new Quat4f( 0f, -0.3f, 0.0f, 1f);
+    quats[5] = new Quat4f( 0f, -0.3f, 0f, 1f);
+    quats[6] = new Quat4f( 0f, -0.2f, 0f, 1f);
+    quats[7] = new Quat4f( 0f, -0.1f, 0.2f, 1f);
+    quats[8] = new Quat4f( 0f, 0.1f, 0.40f, 1f);
+    quats[9] = new Quat4f( 0f, 0.1f, 0.6f, 1f);
+    quats[10] = new Quat4f( 0f, 0.05f, 0.9f, 1f);
+    quats[11] = new Quat4f( 0f, 0.0f, 1.2f, 1f);
+    quats[12] = new Quat4f( 0f, 0f, 1.5f, 1f);
+    quats[13] = new Quat4f( 0f, 0f, 1.7f, 1f);
+    quats[14] = new Quat4f( 0f, 0f, 1.9f, 1f);
+    quats[15] = new Quat4f( 0f, 0f, 2.1f, 1f);
+    quats[16] = new Quat4f( 0f, 0f, 2.2f, 1f);
+    quats[17] = new Quat4f( 0f, 0f, 2.0f, 1f);
+    quats[18] = new Quat4f( 0f, 0f, 2.0f, 1f);
+    quats[19] = new Quat4f( 0f, 0f, 2.0f, 1f);
+    quats[20] = new Quat4f( 0f, 0f, 2.0f, 1f);
+    
+    Point3f[] positions = new Point3f[21];
+    positions[0]= new  Point3f( 0f, 0f, 0f);
+    positions[1]= new  Point3f( 2f, 0f, 0f);
+    positions[2]= new  Point3f( 4f, 0f, 0f);
+    positions[3]= new  Point3f( 6f, 0.1f, 0.0f);
+    positions[4]= new  Point3f( 7f, 0.2f, 0.3f);
+    positions[5]= new  Point3f( 8f, 0.3f, 1.0f);
+    positions[6]= new  Point3f( 9f, 0.4f, 1.5f);
+    positions[7]= new  Point3f( 10f, 0.5f, 1.0f);
+    positions[8]= new  Point3f( 11f, 0.6f, 0.5f);
+    positions[9]= new  Point3f( 12f, 0.7f, 0.0f);
+    positions[10]= new Point3f( 13f, 0.8f, 0.0f);
+    positions[11]= new Point3f( 14f, 0.9f, 0f);
+    positions[12]= new Point3f( 15f, 1.0f, 0f);
+    positions[13]= new Point3f( 16f, 1.2f, 0f);
+    positions[14]= new Point3f( 16.4f, 1.4f, 0.0f);
+    positions[15]= new Point3f( 16.6f, 1.6f, 0.0f);
+    positions[16]= new Point3f( 16.7f, 1.8f, 0.0f);
+    positions[17]= new Point3f( 16.8f, 2.0f, 0.0f);
+    positions[18]= new Point3f( 16.8f, 2.1f, 0.0f);
+    positions[19]= new Point3f( 16.8f, 2.2f, 0.0f);
+    positions[20]= new Point3f( 16.8f, 2.3f, 0.0f);
+   
+    RotPosPathInterpolator carPPI = new RotPosPathInterpolator(carAlpha,tgVei,axisVei,knots,quats,positions);
+    
+    
     BoundingSphere boundss = new BoundingSphere(new Point3d(0.0,0.0,0), 1.0);
-    posIPCar.setSchedulingBounds(boundss);
+    
+   
+    carPPI.setSchedulingBounds(boundss);
     
     //Add the car movement to the group
     tgVei.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-    tgVei.addChild(posIPCar);
-
+    //tgVei.addChild(posIPCar);
+    tgVei.addChild(carPPI);
     
     
     
@@ -429,7 +529,10 @@ public class Trabalho3 extends JFrame
     theScene.addChild(tgVei);
     theScene.addChild(tgGas);
     theScene.addChild(tgTree);
-
+    theScene.addChild(tgRamp);
+    theScene.addChild(tgObs);
+    theScene.addChild(tgTree2);
+    
     //The bounding region for the background.
     BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0),Double.MAX_VALUE);
     //Load the background image.
@@ -466,17 +569,17 @@ public class Trabalho3 extends JFrame
 
     BranchGroup bgLight = new BranchGroup();
 
-    BoundingSphere boundsLight = new BoundingSphere(new Point3d(0.0,0.0,7.0), 10);
+    BoundingSphere boundsLight = new BoundingSphere(new Point3d(0.0,0.0,5.0), 8);
 
 
 
 
     //Light no. 1: directional light.
-    Color3f lightColour1 = new Color3f(0.8f, 0.8f, 0.8f);
+    Color3f lightColour1 = new Color3f(0.0f, 0.0f, 1.0f);
     Vector3f lightDir1  = new Vector3f(0.0f, 0.0f, -0.1f);
     DirectionalLight light1 = new DirectionalLight(lightColour1, lightDir1);
     light1.setInfluencingBounds(boundsLight);
-   bgLight.addChild(light1);
+    bgLight.addChild(light1);
 
 
 
@@ -486,7 +589,7 @@ public class Trabalho3 extends JFrame
                                        new Point3f(1.0f,1.0f,1.0f),
                                        new Point3f(0.2f,0.01f,0.01f));
     light2.setInfluencingBounds(boundsLight);
-    //bgLight.addChild(light2);
+    bgLight.addChild(light2);
 
 
     //Light no. 3: a spotlight.
